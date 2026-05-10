95406f80bc81b690a9c3c5215d087b56f2f4958e
João Marcos <joaomarcoscprofissional@gmail.com>
2026-05-09 21:17:40 -0300

    Testando estrutura

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -1,5 +1,3 @@
-package Java;
-
 public class HelloWorld {
 
     public static void main(String[] args)  {
---
+++ src/src/SomaSimples.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SomaSimples {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int value1 = input.nextInt();
+        int value2 = input.nextInt();
+        int soma = value1 + value2;
+
+        System.out.println("SOMA = "+soma);
+    }
+
+}
