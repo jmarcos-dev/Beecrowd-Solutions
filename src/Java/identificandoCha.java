b5f5898790f16f720b9cb4a7ab99ac16e3aab308
João Marcos <joaomarcoscprofissional@gmail.com>
2026-05-10 00:37:47 -0300

    Feito 2006

---
+++ src/Java/identificandoCha.java
@@ -0,0 +1,31 @@
+package Java;
+
+import java.util.Scanner;
+
+public class identificandoCha {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int primeiro = input.nextInt();
+        int primeiraResposta = input.nextInt();
+        int segundaResposta = input.nextInt();
+        int terceiraResposta = input.nextInt();
+        int quartaResposta = input.nextInt();
+        int quintaResposta = input.nextInt();
+        int contador = 0;
+        if (primeiraResposta == primeiro) {
+            contador++;
+        }  if (segundaResposta == primeiro) {
+            contador++;
+        }  if (terceiraResposta == primeiro) {
+            contador++;
+        }  if (quartaResposta == primeiro) {
+            contador++;
+        }  if (quintaResposta == primeiro) {
+            contador++;
+        }
+        System.out.println(contador);
+        input.close();
+    }
+
+}
