8dbc1b5667895cdef9a83395ade70971daa2c70b
João Marcos <79798116+jmarcos-dev@users.noreply.github.com>
2026-05-13 14:38:38 +0000

    Resolução da problema 1044

---
+++ src/Java/Multiplos.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Multiplos {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int aux = 0;
+        
+        if( B > A)
+            {
+                aux = A;
+                A = B; 
+                B = aux;
+                
+            }
+        int resto = A%B;
+        if(resto==0)
+        {
+            System.out.println("Sao Multiplos");
+        }
+        else
+        {
+            System.out.println("Nao sao Multiplos");
+        }
+    }
+}
