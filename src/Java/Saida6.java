cb86472b5079ae02285426ccfd7e4b2abb068ad1
João Marcos <79798116+jmarcos-dev@users.noreply.github.com>
2026-05-22 22:38:09 -0300

    Add Saida6.java with print statements

---
+++ src/Java/Saida6.java
@@ -0,0 +1,16 @@
+package Java;
+
+
+public class Saida6 {
+    public static void main(String[] args) {
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<    AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<AMO FAZER EXERCICIO >");
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<AMO FAZER EXERCICIO NO URI    >");
+        System.out.println("<AMO FAZER EXERCICIO NO URI>");
+        System.out.println("<          AMO FAZER EXERCICIO >");
+        System.out.println("<AMO FAZER EXERCICIO           >");
+    }
+    
+}
