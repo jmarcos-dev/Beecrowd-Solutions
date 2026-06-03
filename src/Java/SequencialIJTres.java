8c86b0090bb0f6f21c10bab822da71a4117962bf
João Marcos <79798116+jmarcos-dev@users.noreply.github.com>
2026-06-03 14:12:15 +0000

    Solved 1097

---
+++ src/Java/SequencialIJTres.java
@@ -0,0 +1,9 @@
+public class SequencialIJTres {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i += 2) {
+            for (int j = i + 6; j >= i + 4; j--) {
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+    }
+}
