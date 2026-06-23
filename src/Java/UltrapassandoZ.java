ce059d596fcb09f26fe83bdc73f9753eb4247864
João Marcos <joaomarcoscprofissional@gmail.com>
2026-06-23 20:52:38 -0300

    Solved 1150

---
+++ src/Java/UltrapassandoZ.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class UltrapassandoZ {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int X = input.nextInt();
+        int Z = input.nextInt();
+
+        while (Z <= X) {
+            Z = input.nextInt();
+        }
+
+        int soma = 0;
+        int cont = 0;
+        int numero = X;
+
+        while (soma <= Z) {
+            soma += numero;
+            numero++;
+            cont++;
+        }
+
+        System.out.println(cont);
+    }
+}
