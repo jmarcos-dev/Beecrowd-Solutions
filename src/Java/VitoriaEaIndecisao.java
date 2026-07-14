7eb532da48eecaf424b48d27c1e16fb55d04022e
João Marcos <79798116+jmarcos-dev@users.noreply.github.com>
2026-07-14 11:19:51 +0000

    Solved 1924

---
+++ src/Java/VitoriaEaIndecisao.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VitoriaEaIndecisao {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cases = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < cases; i++) {
+            String nomes = sc.nextLine();
+        }
+        System.out.println("Ciencia da Computacao");
+        sc.close();
+    }
+}
