315265204d4c28308814a1056a232635cf9c7c2f
João Marcos <79798116+jmarcos-dev@users.noreply.github.com>
2026-07-15 14:07:19 +0000

    Solved 1189

---
+++ src/Java/AreaEsquerda.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class AreaEsquerda {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char operacao = sc.next().charAt(0);
+        double[][] matriz = new double[12][12];
+        double soma = 0.0;
+        int cont = 0;
+
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                matriz[i][j] = sc.nextDouble();
+
+                if (i > j && i + j < 11) {
+                    soma += matriz[i][j];
+                    cont++;
+
+                }
+            }
+
+        }
+        if (operacao == 'S') {
+            System.out.printf("%.1f%n", soma);
+        } else {
+            System.out.printf("%.1f%n", soma / cont);
+        }
+        sc.close();
+    }
+}
