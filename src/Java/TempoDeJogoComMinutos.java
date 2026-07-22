556345bc05147af2f614ab453fe97726b337708a
João Marcos <79798116+jmarcos-dev@users.noreply.github.com>
2026-07-22 13:20:11 +0000

    Solved 1047

--- src/Java/TempoDeJogoComMinutos.java
+++ src/Java/TempoDeJogoComMinutos.java
@@ -1,19 +1,31 @@
+
 import java.util.Scanner;
 
 public class TempoDeJogoComMinutos {
-    public static void main(String[] arg) {
-        Scanner input = new Scanner(System.in);
-        int hi = input.nextInt();
-        int mi = input.nextInt();
-        int hf = input.nextInt();
-        int mf = input.nextInt();
-        int conversaoInicial = (hi*60)+mi;
-        int conversaoFinal = (hf*60)+mf;
-        int duracao = 0;
-        if (conversaoFinal > conversaoInicial) {
-            duracao = 0;
-        }
 
-        input.close();
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int hi = sc.nextInt();
+        int mi = sc.nextInt();
+        int hf = sc.nextInt();
+        int mf = sc.nextInt();
+        int horaInicial = (hi * 60) + mi;
+        int horaFinal = (hf * 60) + mf;
+        int duracaoMinutos = (horaFinal - horaInicial);
+        int convert = duracaoMinutos / 60;
+        int convertMin = duracaoMinutos % 60;
+
+        if (horaInicial > horaFinal) {
+            duracaoMinutos = ((horaFinal + 1440) - horaInicial);
+            convert = duracaoMinutos / 60;
+            convertMin = duracaoMinutos % 60;
+            System.out.println("O JOGO DUROU " + convert + " HORA(S) E " + convertMin + " MINUTO(S)");
+
+        } else if (duracaoMinutos == 0) {
+            System.out.println("O JOGO DUROU " + 24 + " HORA(S) E " + 0 + " MINUTO(S)");
+        } else {
+            System.out.println("O JOGO DUROU " + convert + " HORA(S) E " + convertMin + " MINUTO(S)");
+
+        }
     }
 }
