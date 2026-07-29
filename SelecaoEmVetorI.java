1c7c70df5e0bb6b518cdc5dab129388573ea9bd4
João Marcos <79798116+jmarcos-dev@users.noreply.github.com>
2026-07-29 13:42:14 +0000

    Solved 1174

---
+++ SelecaoEmVetorI.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class SelecaoEmVetorI {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] vetor = new double[100];
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = sc.nextDouble();
+
+        }
+        for (int i = 0; i < 100; i++) {
+            if (vetor[i] <= 10) {
+                System.out.println("A[" + i + "] = " + vetor[i]);
+            }
+
+        }
+
+        sc.close();
+    }
+}
