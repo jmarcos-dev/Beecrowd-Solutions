92eb9e7cf1c38a0886f9bfe09cec4d4ab175eb62
João Marcos <joaomarcos.c.dev@gmail.com>
2026-07-29 20:56:07 -0300

    Solved 2483

---
+++ src/Java/FelizNataaal.java
@@ -0,0 +1,14 @@
+package Java;
+import java.util.Scanner;
+
+
+public class FelizNataaal {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Feliz nat" + "a".repeat(x) + "l!");
+        sc.close();
+    }
+
+}
---
+++ src/Java/IamToorg.java
@@ -0,0 +1,15 @@
+package Java;
+
+import java.util.Scanner;
+
+public class IamToorg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cases = sc.nextInt();
+        for (int i = 0; i < cases; i++) {
+            String question = sc.next();
+        }
+
+        sc.close();
+    }
+}
