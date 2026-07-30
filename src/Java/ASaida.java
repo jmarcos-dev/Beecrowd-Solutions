f411fe7243a511ee915b15f6797764b0bd0f85ef
João Marcos <joaomarcos.c.dev@gmail.com>
2026-07-29 21:45:49 -0300

    Solved 2861

---
+++ src/Java/ASaida.java
@@ -0,0 +1,17 @@
+package Java;
+
+import java.util.Scanner;
+
+public class ASaida {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cases = sc.nextInt();
+        String questions = new String();
+        for (int i = 0; i < cases; i++) {
+            sc.next();
+            questions = sc.nextLine();
+            System.out.println("gzuz");
+        }
+        sc.close();
+    }
+}
